3f4c5ffac4df6e5f7221a2b273197d5f3f1a9cb7
Sofia Muller <sofia.muller@hotmail.com>
2018-08-08 13:02:00 -0500

    delete starter code

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,52 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// Select table body
+var tbody = d3.select("tbody");
+
+// Loop Through `data` to add rows, cells and text
+data.forEach((sighting) => {
+    var row = tbody.append("tr");
+    Object.entries(sighting).forEach(([key, value]) => {
+        var cell = tbody.append("td");
+        cell.text(value);
+    });
+});
+
+// Define a variable for the filter button
+var filter = d3.select("#filter-btn");
+
+filter.on("click", function() {
+    // Prevent the page from refreshening
+    d3.event.preventDefault();
+
+    var inputDate = d3.select("#datetime");
+    var inputValue = inputDate.property("value");
+    console.log(inputValue);
+    var filteredReport = data.filter(sighting => sighting.datetime === inputValue);
+    console.log(filteredReport);
+
+    // If they enter without putting anything in the search button, rebuild the table;
+    // If they input a date not found in the table, let them know; otherwise build the table from filterdata.
+    
+    if (filteredReport.length > 0) {
+        tbody.html("");
+        filteredReport.forEach((sighting) => {
+            var row = tbody.append("tr");
+            Object.entries(sighting).forEach(([key, value]) => {
+                var cell = tbody.append("td");
+                cell.text(value);
+            });
+        });
+    }
+    else {
+        alert(`${inputValue} not found.`);
+        data.forEach(function(sighting) {
+            var row = tbody.append("tr");
+            Object.entries(sighting).forEach(([key, value]) => {
+                var cell = tbody.append("td");
+                cell.text(value);
+            });
+        });
+    }
+});
